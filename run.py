5816603f8fb8440052779fbed68b83b6dc6e92b0
alexis <git@alexistats.com>
2025-03-08 09:29:43 -0500

    added progressions and user login

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from app import create_app, db
-from app.models import User, Workout, ExerciseLog
+from app.models import User, Workout, ExerciseLog, UserProgression
 
 app = create_app()
-
+with app.app_context():
+    db.create_all()
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Workout': Workout, 'ExerciseLog': ExerciseLog}
